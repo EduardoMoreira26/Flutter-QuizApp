9c48536eb1a8af4545e44dc95cc8b953a1ed9766
Eduardo Moreira <Du_du.mba@hotmail.com>
2021-10-21 09:59:34 -0300

    create question component

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './resposta.dart';
-
+import './questionario.dart';
+import './resultado.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -21,9 +20,6 @@ class _PerguntaAppState extends State<PerguntaApp>{
       'respostas': ['Léo', 'Pedro', 'Eduardo', 'Juliana'],
   }];
 
-  bool get temPerguntaSelecionada {
-    return _perguntaSelecionada < _perguntas.length;
-  }
 
   void _responder(){
     if(temPerguntaSelecionada){
@@ -33,30 +29,27 @@ class _PerguntaAppState extends State<PerguntaApp>{
     }
   }
 
+   bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
   @override
   Widget build(BuildContext context) {
    
-    List<String> respostas = temPerguntaSelecionada ? _perguntas[_perguntaSelecionada]['respostas'] : null;
+    
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: temPerguntaSelecionada ? Column(
-          children: [
-            Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
-            ...respostas.map((t) => Resposta(t, _responder)).toList(),
-          ],
-        ) : Center(
-          child: Text(
-            'Parabéns!!!',
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 36
-            ),
-          ),
-        ),
+        body: temPerguntaSelecionada ?
+        Questionario(
+          perguntas: _perguntas,
+          perguntaSelecionada: _perguntaSelecionada,
+          quandoResponder: _responder,
+        )
+        : Resultado('Parabéns!')
       ),
     );
   }
---
+++ lib/resultado.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Resultado  extends StatelessWidget {
+  final String text;
+
+  Resultado(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.blue,
+          fontSize: 36
+        ),
+      ),
+    );
+  }
+
+}
