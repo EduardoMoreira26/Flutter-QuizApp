6aadf376dadc584081ca32487f40c82973a52e2a
Eduardo Moreira <Du_du.mba@hotmail.com>
2021-10-21 08:49:51 -0300

    create map

--- lib/main.dart
+++ lib/main.dart
@@ -8,44 +8,54 @@ main() => runApp(PerguntaApp());
 class _PerguntaAppState extends State<PerguntaApp>{
   var _perguntaSelecionada = 0;
 
+  final List _perguntas = const [{
+      'texto': 'Qual é sua cor favorita?',
+      'respostas': ['Preto', 'Azul', 'Verde', 'Vermelho'],
+    },
+    {
+      'texto':'Qual é sua animal favorito?',
+      'respostas': ['Leão', 'Elefante', 'Coelho', 'Cachorro'],
+    },
+    {
+      'texto':'Qual é seu instrutor preferido?',
+      'respostas': ['Léo', 'Pedro', 'Eduardo', 'Juliana'],
+  }];
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
   void _responder(){
+    if(temPerguntaSelecionada){
     setState(() {
       _perguntaSelecionada++;
-        });
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final List perguntas = [{
-        'texto': 'Qual é sua cor favorita?',
-        'respostas': ['Preto', 'Azul', 'Verde', 'Vermelho'],
-      },
-      {
-        'texto':'Qual é sua animal favorito?',
-        'respostas': ['Leão', 'Elefante', 'Coelho', 'Cachorro'],
-      },
-      {
-        'texto':'Qual é seu instrutor preferido?',
-        'respostas': ['Léo', 'Pedro', 'Eduardo', 'Juliana'],
-    }];
-
-    List<String> respostas = perguntas[_perguntaSelecionada]['respostas'];
-    List<Widget> widgets = respostas.map((t) => Resposta(t, _responder)).toList();
-
-    // for (String textoResposta in respostas) {
-    //   widgets.add(Resposta(textoResposta, _responder));
-    // }
+   
+    List<String> respostas = temPerguntaSelecionada ? _perguntas[_perguntaSelecionada]['respostas'] : null;
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Column(
+        body: temPerguntaSelecionada ? Column(
           children: [
-            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
-            ...widgets,
+            Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
+            ...respostas.map((t) => Resposta(t, _responder)).toList(),
           ],
+        ) : Center(
+          child: Text(
+            'Parabéns!!!',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 36
+            ),
+          ),
         ),
       ),
     );
